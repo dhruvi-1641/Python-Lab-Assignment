41946a9c7ae9586ac797ece55e69b4f767841e1b
dhruvi-1641 <bca2022dhruvi1641@tnraocollege.org>
2024-07-13 16:03:22 +0530

    Add files via upload

---
+++ python lab assi/employee.py
@@ -0,0 +1,13 @@
+EMPNAME = []
+
+EMPNAME. append("Dhruvi")
+EMPNAME. append("Isha")
+EMPNAME.append("Hasti")
+
+print("Employee Names after additions:", EMPNAME)
+EMPNAME.remove("Isha")
+
+print("Employee Names after removal  :", EMPNAME)
+EMPNAME. append("Happy")
+
+print("Final Employee Names          :", EMPNAME)
